7f78120f25faa429b472ebb1a95b91c32fe84842
Mayuresh-Kb <mayureshkasabe51@gmail.com>
2026-07-27 23:47:52 +0530

    ResNet50 model implemented

--- ResNet-50/model.py
+++ ResNet-50/model.py
@@ -1,3 +1,99 @@
 import torch
 import torch.nn as nn
 
+class Bottleneck(nn.Module):
+    expansion = 4
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super().__init__()
+        self.identity_downsample = identity_downsample
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels*Bottleneck.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out_channels*Bottleneck.expansion)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        
+        x += identity
+        x = self.relu(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self, bottleneck, layers, image_channels, num_classes):
+        super().__init__()
+
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # layers
+        self.layer1 = self._make_layer(bottleneck, layers[0], 64, stride=1)
+        self.layer2 = self._make_layer(bottleneck, layers[1], 128, stride=2)
+        self.layer3 = self._make_layer(bottleneck, layers[2], 256, stride=2)
+        self.layer4 = self._make_layer(bottleneck, layers[3], 512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512*4, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+    def _make_layer(self, bottleneck, num_residual_blocks, out_channels, stride):
+        
+        identity_downsampling = None
+        layers=[]
+
+        if stride != 1 or self.in_channels != out_channels*4:
+            identity_downsampling = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels*4, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels*4)
+            )
+        
+        layers.append(bottleneck(self.in_channels, out_channels, identity_downsampling, stride))
+        self.in_channels = out_channels * 4
+
+        for i in range(num_residual_blocks - 1):
+            layers.append(bottleneck(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+def ResNet50(img_channels=3, num_classes=1000):
+    return ResNet(Bottleneck, [3,4,6,3], img_channels, num_classes)
+
+def test():
+    net = ResNet50()
+    x = torch.randn(2, 3, 224, 224)
+    y = net(x)
+    print(y.shape)
+
+test()
